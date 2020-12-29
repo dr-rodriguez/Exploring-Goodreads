56906e08cc62581279ef37ec475e9601897fe2b1
dr-rodriguez <strakul@gmail.com>
2020-12-29 16:26:41 -0500

    updating wordcloud

--- word_cloud.py
+++ word_cloud.py
@@ -1,15 +1,18 @@
 # Word Cloud of my reviews
 
 import pickle
-import pandas as pd
+import numpy as np
+from PIL import Image
 from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-from scipy.misc import imread
 import matplotlib.pyplot as plt
 from utilities import my_replacements
 
 # Load data extracted from API (118 reviews)
-with open('data/reviews.pkl','r') as f:
+with open('data/books.pkl', 'rb') as f:
     df = pickle.load(f)
+
+# Remove non-reviews
+df = df[~df['text'].isnull()]
 
 filename = 'figures/wordcloud.png'
 size = 10
@@ -18,6 +21,7 @@ horizontal = 0.9
 image = 'images/book_clipart3.png'
 mask = 'images/book_clipart3.png'
 
+words = ''
 words = ' '.join(df['text'].tolist())
 
 # Clean up the text
@@ -37,17 +41,14 @@ stopwords.add('amp')
 stopwords.add('lt')
 
 # Load up a logo as a mask & color image
-cloud_mask = imread(mask)
-logo = imread(image)
+logo = np.array(Image.open(image))
 
 # Generate colors
 image_colors = ImageColorGenerator(logo)
 
 # Generate plot
-wc = WordCloud(stopwords=stopwords, mask=cloud_mask, color_func=image_colors, scale=0.8,
+wc = WordCloud(stopwords=stopwords, mask=logo, color_func=image_colors, scale=0.8,
                max_words=max_words, background_color='white', random_state=42, prefer_horizontal=horizontal)
-# wc = WordCloud(stopwords=stopwords, scale=0.8, max_words=max_words, background_color='white',
-#                random_state=42, prefer_horizontal=horizontal)
 
 wc.generate(words_no_urls)
 
